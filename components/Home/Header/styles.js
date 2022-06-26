3aa4d1874f04705ac55e7a3af0f0d60bd46bddb3
JaiRaga <44367062+JaiRaga@users.noreply.github.com>
2022-06-26 14:58:12 +0530

    :art: added styling for unread badge and modified styles for logo

--- components/Home/Header/styles.js
+++ components/Home/Header/styles.js
@@ -7,16 +7,35 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     marginHorizontal: 20,
   },
+  logo: {
+    width: 100,
+    height: 50,
+    borderRadius: 25,
+    borderWidth: 1,
+    borderColor: 'white',
+    resizeMode: 'cover',
+  },
   iconsContainer: {
     flexDirection: 'row',
   },
   icon: {
-    marginHorizontal: 10
+    marginHorizontal: 10,
   },
-  logo: {
-    width: 100,
-    height: 50,
-    resizeMode: 'cover',
+  unreadBadge: {
+    backgroundColor: '#0093AB',
+    position: 'absolute',
+    left: 20,
+    bottom: 18,
+    width: 25,
+    height: 18,
+    borderRadius: 9,
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 100
+  },
+  unreadBadgeText: {
+    color: '#ffffff',
+    fontWeight: "600"
   },
 });
 
