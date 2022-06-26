181b873bf8b224ab03402d82c28e733169f9b366
JaiRaga <44367062+JaiRaga@users.noreply.github.com>
2022-06-26 18:52:51 +0530

    :zap: holds dummy data

---
+++ assets/data/posts.js
@@ -0,0 +1,148 @@
+import { users } from './users';
+
+export const posts = [
+  {
+    imageUrl:
+      'https://images.pexels.com/photos/414061/pexels-photo-414061.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    user: users[0].user,
+    likes: 7990,
+    caption: 'enjoying life',
+    profilePicture: users[0].image,
+    comments: [
+      { user: 'the_man', comment: 'Nice Scenery!' },
+      { user: 'this_guy', comment: 'You ok bro!' },
+    ],
+  },
+  {
+    imageUrl:
+      'https://images.pexels.com/photos/532168/pexels-photo-532168.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    user: users[1].user,
+    likes: 7990,
+    caption: 'gardening!!!',
+    profilePicture: users[1].image,
+    comments: [
+      { user: 'the_man', comment: 'Nice!' },
+      { user: 'this_guy', comment: 'Sup bro!' },
+    ],
+  },
+  {
+    imageUrl:
+      'https://images.pexels.com/photos/414061/pexels-photo-414061.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    user: users[0].user,
+    likes: 7990,
+    caption: 'enjoying life',
+    profilePicture: users[0].image,
+    comments: [
+      { user: 'the_man', comment: 'Nice Scenery!' },
+      { user: 'this_guy', comment: 'You ok bro!' },
+    ],
+  },
+  {
+    imageUrl:
+      'https://images.pexels.com/photos/532168/pexels-photo-532168.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    user: users[1].user,
+    likes: 7990,
+    caption: 'gardening!!!',
+    profilePicture: users[1].image,
+    comments: [
+      { user: 'the_man', comment: 'Nice!' },
+      { user: 'this_guy', comment: 'Sup bro!' },
+    ],
+  },
+  {
+    imageUrl:
+      'https://images.pexels.com/photos/414061/pexels-photo-414061.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    user: users[0].user,
+    likes: 7990,
+    caption: 'enjoying life',
+    profilePicture: users[0].image,
+    comments: [
+      { user: 'the_man', comment: 'Nice Scenery!' },
+      { user: 'this_guy', comment: 'You ok bro!' },
+    ],
+  },
+  {
+    imageUrl:
+      'https://images.pexels.com/photos/532168/pexels-photo-532168.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    user: users[1].user,
+    likes: 7990,
+    caption: 'gardening!!!',
+    profilePicture: users[1].image,
+    comments: [
+      { user: 'the_man', comment: 'Nice!' },
+      { user: 'this_guy', comment: 'Sup bro!' },
+    ],
+  },
+  {
+    imageUrl:
+      'https://images.pexels.com/photos/414061/pexels-photo-414061.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    user: users[0].user,
+    likes: 7990,
+    caption: 'enjoying life',
+    profilePicture: users[0].image,
+    comments: [
+      { user: 'the_man', comment: 'Nice Scenery!' },
+      { user: 'this_guy', comment: 'You ok bro!' },
+    ],
+  },
+  {
+    imageUrl:
+      'https://images.pexels.com/photos/532168/pexels-photo-532168.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    user: users[1].user,
+    likes: 7990,
+    caption: 'gardening!!!',
+    profilePicture: users[0].image,
+    comments: [
+      { user: 'the_man', comment: 'Nice!' },
+      { user: 'this_guy', comment: 'Sup bro!' },
+    ],
+  },
+  {
+    imageUrl:
+      'https://images.pexels.com/photos/414061/pexels-photo-414061.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    user: users[0].user,
+    likes: 7990,
+    caption: 'enjoying life',
+    profilePicture: users[0].image,
+    comments: [
+      { user: 'the_man', comment: 'Nice Scenery!' },
+      { user: 'this_guy', comment: 'You ok bro!' },
+    ],
+  },
+  {
+    imageUrl:
+      'https://images.pexels.com/photos/532168/pexels-photo-532168.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    user: users[1].user,
+    likes: 7990,
+    caption: 'gardening!!!',
+    profilePicture: users[0].image,
+    comments: [
+      { user: 'the_man', comment: 'Nice!' },
+      { user: 'this_guy', comment: 'Sup bro!' },
+    ],
+  },
+  {
+    imageUrl:
+      'https://images.pexels.com/photos/414061/pexels-photo-414061.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    user: users[0].user,
+    likes: 7990,
+    caption: 'enjoying life',
+    profilePicture: users[0].image,
+    comments: [
+      { user: 'the_man', comment: 'Nice Scenery!' },
+      { user: 'this_guy', comment: 'You ok bro!' },
+    ],
+  },
+  {
+    imageUrl:
+      'https://images.pexels.com/photos/532168/pexels-photo-532168.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    user: users[1].user,
+    likes: 7990,
+    caption: 'gardening!!!',
+    profilePicture: users[0].image,
+    comments: [
+      { user: 'the_man', comment: 'Nice!' },
+      { user: 'this_guy', comment: 'Sup bro!' },
+    ],
+  },
+];
---
+++ assets/data/users.js
@@ -0,0 +1,62 @@
+export const users = [
+  {
+    user: 'andy the sjkjsbjfkbwjgb wsgsdgs gsgsgknsk sngls lsgls lsgbls lsgnls l',
+    image:
+      'https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    user: 'Tom',
+    image:
+      'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    user: 'Bob',
+    image:
+      'https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    user: 'Jess',
+    image:
+      'https://images.pexels.com/photos/460635/pexels-photo-460635.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    user: 'andy',
+    image:
+      'https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    user: 'Tom',
+    image:
+      'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    user: 'Bob',
+    image:
+      'https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    user: 'Jess',
+    image:
+      'https://images.pexels.com/photos/460635/pexels-photo-460635.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    user: 'andy',
+    image:
+      'https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    user: 'Tom',
+    image:
+      'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    user: 'Bob',
+    image:
+      'https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    user: 'Jess',
+    image:
+      'https://images.pexels.com/photos/460635/pexels-photo-460635.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+];
