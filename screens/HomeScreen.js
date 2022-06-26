525b651d211b86162da58b35e9a1fea3ed0a6518
JaiRaga <44367062+JaiRaga@users.noreply.github.com>
2022-06-26 18:53:38 +0530

    :zap: displays header, stories and posts

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import Header from '../components/Home/Header';
+import Stories from '../components/Home/Stories';
+import Posts from '../components/Home/Posts';
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
       <Header />
+      <Stories />
+      <Posts />
     </View>
   );
 };
